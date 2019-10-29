62072a4ca2246f7f6858e0f4fe5f8092cd5dd608
skathanas <32923297+skathanas@users.noreply.github.com>
2019-10-29 12:57:52 +0200

    Add files via upload

---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Palindrome
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Teretulemast palindroomi programmi");
+            Console.WriteLine("Palun kirjuta oma nimi:");
+            var original = Console.ReadLine();
+            var reversed = new string(original.Reverse().ToArray());
+            if (original == reversed) {
+                Console.WriteLine("Teie nimi tagurpidi on " + reversed + ". Ning tegu on palindroomiga");
+            }
+            else
+            { Console.WriteLine("Teie nimi tagurpidi on " + reversed + ". Ning tegu pole palindroomiga");
+            }
+        }
+    }
+}
